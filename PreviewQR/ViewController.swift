91fc2bbbd81d275161d033d3125d05bd4c27181a
Mario Reyes Ojeda <mfreyesojeda@gmail.com>
2017-06-01 22:07:20 -0500

    Initial Commit

---
+++ PreviewQR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PreviewQR
+//
+//  Created by Mario Reyes Ojeda on 1/06/17.
+//  Copyright © 2017 Mario Reyes Ojeda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
